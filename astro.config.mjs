41fe0d64729ac539ca5a4816372598a1a9894908
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-11 15:37:53 +0900

    clean: rm unnecessary code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
-  // output: "server",
-  // adapter: netlify()
 });
