6f7fb04935055802fc0db96e5d9e2e776b7b2c0e
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-11 14:57:10 +0900

    test:netlify deploy without settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify()
+  // output: "server",
+  // adapter: netlify()
 });
