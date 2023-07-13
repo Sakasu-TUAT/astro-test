823e5b4420b651a23ee438121374270194d6a4b3
Sakasu02 <s214229y@st.go.tuat.ac.jp>
2023-07-13 17:13:48 +0900

    add: hamburger menu

---
+++ src/utils/hamburger.js
@@ -0,0 +1,11 @@
+let nav = document.querySelector("#navArea");
+let btn = document.querySelector(".toggle-btn");
+let mask = document.querySelector("#mask");
+
+btn.onclick = () => {
+  nav.classList.toggle("open");
+};
+
+mask.onclick = () => {
+  nav.classList.toggle("open");
+};
